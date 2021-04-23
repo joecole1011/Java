cc1a2aff8954beb4d39d2bc7258d3e7daca46a05
joecole1011 <akawit_7@hotmail.com>
2021-04-23 11:47:38 +0700

    Lab 5
    Lab 5 v.1

---
+++ HelloWorld/src/com/kimzun/helloWord/Lab5.java
@@ -0,0 +1,29 @@
+package com.kimzun.helloWord;
+
+public class Lab5 {
+
+	public static void main(String[] args) {
+		int number = 60;
+		switch (number) {
+		case 80:
+			System.out.println("Gread A");
+			break;
+		case 70:
+			System.out.println("Gread B");
+			break;
+		case 60:
+			System.out.println("Gread C");
+			break;
+		case 50:
+			System.out.println("Gread D");
+			break;
+		case 40:
+			System.out.println("Gread F");
+			break;
+		default:
+			System.out.println("Gread E");
+		}
+
+	}
+
+}
