1d6f8b79333cc5e9ecc2403770b615d79a2bebc2
joecole1011 <akawit_7@hotmail.com>
2021-04-23 19:43:06 +0700

    Lab 12_1
    Lab 12_1 v.1

---
+++ HelloWorld/src/com/kimzun/helloWord/Lab12_1.java
@@ -0,0 +1,10 @@
+package com.kimzun.helloWord;
+
+public class Lab12_1 {
+
+	public static void main(String[] args) {
+		char[] myChar = { 'A', 'k', 'a', 'w', 'i', 't', ' ', 'N', 'a', 's', 'o', 'k', 'e' };
+		String myWord = new String(myChar);
+		System.out.println(myWord);
+	}
+}
